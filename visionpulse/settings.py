78445ea43e7a2ae6cee644bfc2cda443c285a73f
Edwinrpb <pastoredwin_777@hotmail.com>
2025-10-17 19:18:10 -0500

    Stable

--- visionpulse/settings.py
+++ visionpulse/settings.py
@@ -195,6 +195,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Tailwind CSS
 TAILWIND_APP_NAME = 'theme'
 
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
